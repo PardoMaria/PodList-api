a7c72b45577416653308aa76cc75b46c5f7433ba
Maria Pardo <pardo.genis@gmail.com>
2020-11-01 10:17:05 +0100

    list and like models

--- bin/seeds.js
+++ bin/seeds.js
@@ -2,13 +2,11 @@ require("dotenv").config();
 require("../config/db.config");
 const User = require("../models/User.model");
 const Podcast = require("../models/Podcast.model");
-// const Review = require("../models/Review.model");
 const faker = require("faker");
 
 const userIds = [];
 const userN = 10;
-// const podcastN = 5;
-// const reviewN = 5;
+
 
 Promise.all([User.deleteMany()])
   .then(() => {
